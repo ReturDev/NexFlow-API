1ede690fc6ac31f6fe98147bbd50003fc5a67115
sergiomm50 <sergiomm50@gmail.com>
2026-03-26 19:23:00 +0100

    Initial commit

---
+++ src/main/java/com/returdev/nexflow/NexflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.returdev.nexflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/returdev/nexflow/NexflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.returdev.nexflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
