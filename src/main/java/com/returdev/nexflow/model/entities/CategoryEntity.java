c107bef8297be2d92cfe7f36dcf3b47bef853e46
sergiomm50 <sergiomm50@gmail.com>
2026-03-27 16:08:37 +0100

    feat: implement CategoryEntity for transaction classification
    - Create 'categories' table with unique name constraint.
    - Add icon_resource field for UI mapping.
    - Set name column as non-updatable and non-nullable.
    - Integrate JPA Auditing (@CreatedDate, @LastModifiedDate).

---
+++ src/main/java/com/returdev/nexflow/model/entities/CategoryEntity.java
@@ -0,0 +1,63 @@
+package com.returdev.nexflow.model.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(
+        name = "categories",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"name"}
+                )
+        }
+)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true, updatable = false, nullable = false, length = 50)
+    @NotBlank(message = "{validation.not_blank.message}")
+    private String name;
+
+    @Column(name = "icon_resource")
+    private String iconResource;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CategoryEntity that = (CategoryEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
