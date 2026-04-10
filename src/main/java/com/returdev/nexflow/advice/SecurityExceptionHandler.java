d9fde77a7fc7496f75edbe56ffabfa540559df9e
sergiomm50 <sergiomm50@gmail.com>
2026-04-10 20:34:52 +0200

    refactor: relocate handlers and enforce global precedence
    - Move 'BusinessExceptionHandler' and 'SecurityExceptionHandler' to the correct
      exception handling package.
    - Apply '@Order(Ordered.HIGHEST_PRECEDENCE)' to both handlers.

--- src/main/java/com/returdev/nexflow/advice/SecurityExceptionHandler.java
+++ src/main/java/com/returdev/nexflow/advice/SecurityExceptionHandler.java
@@ -1,8 +1,11 @@
-package com.returdev.nexflow.advice.manager;
+package com.returdev.nexflow.advice;
 
+import com.returdev.nexflow.advice.manager.MessageManager;
 import com.returdev.nexflow.model.exceptions.InvalidPasswordException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SecurityExceptionHandler {
 
     private final MessageManager messageManager;
