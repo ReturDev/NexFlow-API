f88d9886bf3030e81fae0f8e6f69e04738456327
Portatil\Sergio <sergiomm50@gmail.com>
2026-04-20 22:10:08 +0200

    feat: configure LockProvider for ShedLock integration
    - Added 'LockProvider' bean to ApplicationConfig using 'JdbcTemplateLockProvider'.
    - Configured provider to use the application's 'DataSource' and database time for synchronization.

--- src/main/java/com/returdev/nexflow/config/ApplicationConfig.java
+++ src/main/java/com/returdev/nexflow/config/ApplicationConfig.java
@@ -1,11 +1,14 @@
 package com.returdev.nexflow.config;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * General application configuration class.
@@ -14,4 +17,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
 @Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime()
+                        .build()
+        );
+    }
+
 }
